cce447faf6a65b2a31242ce7323583de852b3f08
chris <abc@abchris.xyz>
2022-01-06 09:54:46 +1300

    Fix bank box selection

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -42,9 +42,13 @@ const Calculator = () => {
       const url = 'https://api.ergoplatform.com/api/v0/transactions/boxes/byAddress/unspent/MUbV38YgqHy7XbsoXWF5z7EZm524Ybdwe5p9WDrbhruZRtehkRPT92imXer2eTkjwPDfboa1pR3zb3deVKVq3H7Xt98qcTqLuSBSbHb7izzo5jphEpcnqyKJ2xhmpNPVvmtbdJNdvdopPrHHDBbAGGeW7XYTQwEeoRfosXzcDtiGgw97b2aqjTsNFmZk7khBEQywjYfmoDc9nUCJMZ3vbSspnYo3LarLe55mh2Np8MNJqUN9APA6XkhZCrTTDRZb1B4krgFY1sVMswg2ceqguZRvC9pqt3tUUxmSnB24N6dowfVJKhLXwHPbrkHViBv1AKAJTmEaQW2DN1fRmD9ypXxZk8GXmYtxTtrj3BiunQ4qzUCu1eGzxSREjpkFSi2ATLSSDqUwxtRz639sHM6Lav4axoJNPCHbY8pvuBKUxgnGRex8LEGM8DeEJwaJCaoy8dBw9Lz49nq5mSsXLeoC4xpTUmp47Bh7GAZtwkaNreCu74m9rcZ8Di4w1cmdsiK1NWuDh9pJ2Bv7u3EfcurHFVqCkT3P86JUbKnXeNxCypfrWsFuYNKYqmjsix82g9vWcGMmAcu5nagxD4iET86iE2tMMfZZ5vqZNvntQswJyQqv2Wc6MTh4jQx1q2qJZCQe4QdEK63meTGbZNNKMctHQbp3gRkZYNrBtxQyVtNLR8xEY8zGp85GeQKbb37vqLXxRpGiigAdMe3XZA4hhYPmAAU5hpSMYaRAjtvvMT3bNiHRACGrfjvSsEG9G2zY5in2YWz5X9zXQLGTYRsQ4uNFkYoQRCBdjNxGv6R58Xq74zCgt19TxYZ87gPWxkXpWwTaHogG1eps8WXt8QzwJ9rVx6Vu9a5GjtcGsQxHovWmYixgBU8X9fPNJ9UQhYyAWbjtRSuVBtDAmoV1gCBEPwnYVP5GCGhCocbwoYhZkZjFZy6ws4uxVLid3FxuvhWvQrVEDYp7WRvGXbNdCbcSXnbeTrPMey1WPaXX';
       const response = await fetch(url);
       const json = await response.json();
-      const sigusd = json[0].assets[0].amount / 100;
-      const sigrsv = json[0].assets[1].amount;
-      const nanoERGs = json[0].value
+      // There is only ever 1 unspent bank box, but since some ergold got sent to the bank address,
+      // need to avoid using that box.
+      // Here we drop all boxes not having sigusd as their first asset.
+      const box = json.filter((b) => b.assets[0].tokenId == "03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04")[0];
+      const sigusd = box.assets[0].amount / 100;
+      const sigrsv = box.assets[1].amount;
+      const nanoERGs = box.value
       setBank(createBank(sigusd, sigrsv, nanoERGs))
     }
     fecthBank();
