9ad1ef1763c6dcbba6c444b9a2ba6806ff992172
chris <abc@abchris.xyz>
2022-01-22 09:28:43 +1300

    Fix eqeqeq warning

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -45,7 +45,7 @@ const Calculator = () => {
       // There is only ever 1 unspent bank box, but since some ergold got sent to the bank address,
       // need to avoid using that box.
       // Here we drop all boxes not having sigusd as their first asset.
-      const box = json.filter((b) => b.assets[0].tokenId == "03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04")[0];
+      const box = json.filter((b) => b.assets[0].tokenId === "03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04")[0];
       const sigusd = box.assets[0].amount / 100;
       const sigrsv = box.assets[1].amount;
       const nanoERGs = box.value
